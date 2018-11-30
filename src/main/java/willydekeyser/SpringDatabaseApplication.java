1f0855228673cb8c355d996f01bd411413418e32
Willy De Keyser <willy.de.keyser@skynet.be>
2018-11-30 10:14:40 +0100

    Update 30/11/2018
    Signed-off-by: Willy De Keyser <willy.de.keyser@skynet.be>

--- src/main/java/willydekeyser/SpringDatabaseApplication.java
+++ src/main/java/willydekeyser/SpringDatabaseApplication.java
@@ -1,5 +1,11 @@
 package willydekeyser;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,6 +17,28 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class SpringDatabaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDatabaseApplication.class, args);	
+		SpringApplication.run(SpringDatabaseApplication.class, args);
+		
+		MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model;
+		try {
+			model = reader.read(new FileReader("pom.xml"));
+			System.out.println(model.getId());
+	        System.out.println(model.getGroupId());
+	        System.out.println(model.getArtifactId());
+	        System.out.println(model.getVersion());
+		} catch (IOException | XmlPullParserException e) {
+			System.err.println("ERROR " + e.getMessage());
+		}
+        
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
--- src/main/java/willydekeyser/security/MyAuthenticationSuccessHandler.java
+++ src/main/java/willydekeyser/security/MyAuthenticationSuccessHandler.java
@@ -9,13 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
-	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	//private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 	
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -28,6 +26,9 @@ public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHand
 			//} catch (IOException e) {
 			//	System.out.println("ERROR: " + e.getMessage());;
 			//}
+			
+
+			
 	});
 	}
 }
