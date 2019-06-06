194bea023915f933cd36bbd401c49b72ada9f85c
Willy De Keyser <willy.de.keyser@skynet.be>
2019-06-06 11:29:10 +0200

    Update Aspect

--- src/main/java/willydekeyser/aspect/LoggingAspect.java
+++ src/main/java/willydekeyser/aspect/LoggingAspect.java
@@ -1,11 +1,14 @@
 package willydekeyser.aspect;
 
+import java.util.Enumeration;
+
 import javax.security.auth.message.AuthException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -50,41 +53,41 @@ public class LoggingAspect {
 		System.out.println("Aspect run MODEL: " + joitPoint.getSignature().getDeclaringTypeName() + " - " + joitPoint.getSignature().getName());
 	}
 	
-	@Before("execution(* willydekeyser.controller.LedenController.*(..))")
+	//@Before("execution(* willydekeyser.controller.LedenController.*(..))")
 	public void AdviceAll(JoinPoint joitPoint) throws AuthException {
 		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-		//System.err.println("Aspect run ALL: " + joitPoint.getSignature().getDeclaringTypeName() + " - " + joitPoint.getSignature().getName());
-		//System.err.println("REQUEST: " + request.getUserPrincipal().getName() + " - " + request.getLocalName());
-		//Enumeration<String> headerNames = request.getHeaderNames();
-		//String test_header = request.getHeader("header");
-		//System.out.println("HEADER voor: " + test_header);
-		//if (test_header.isEmpty() || test_header == null) {
-		//	System.out.println("HEADER: " + test_header);
-		//	throw new IllegalAccessError("Header ERROR");
-		//}
-		//System.err.println("HEADERS: ---------------------------------------- ");
-		//while (headerNames.hasMoreElements()) {
-		//	String headerName = headerNames.nextElement();
-		//	System.err.print("Header: " + headerName);
-		//	System.err.print(" ");
-		//	Enumeration<String> headers = request.getHeaders(headerName);
-		//	while (headers.hasMoreElements()) {
-		//		String headerValue = headers.nextElement();
-		//		System.err.print(" " + headerValue);
-		//		System.err.println(" ");
-		//	}
-		//}
-		String xAuth = request.getHeader("header");
-		if (xAuth == null) {
-			throw new AuthException("Exception message from AOP on unauthorized access NULL");
-		} {
-			if (!xAuth.equals("Willy De Keyser")) {
-				throw new AuthException("Exception message from AOP on unauthorized access");
+		System.err.println("Aspect run ALL: " + joitPoint.getSignature().getDeclaringTypeName() + " - " + joitPoint.getSignature().getName());
+		System.err.println("REQUEST: " + request.getUserPrincipal().getName() + " - " + request.getLocalName());
+		Enumeration<String> headerNames = request.getHeaderNames();
+		String test_header = request.getHeader("header");
+		System.out.println("HEADER voor: " + test_header);
+		if (test_header == null) {
+			System.out.println("HEADER: " + test_header);
+			//throw new AuthException("Header ERROR");
+		}
+		System.err.println("HEADERS: ---------------------------------------- ");
+		while (headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			System.err.print("Header: " + headerName);
+			System.err.print(" ");
+			Enumeration<String> headers = request.getHeaders(headerName);
+			while (headers.hasMoreElements()) {
+				String headerValue = headers.nextElement();
+				System.err.print(" " + headerValue);
+				System.err.println(" ");
 			}
 		}
+//		String xAuth = request.getHeader("header");
+//		if (xAuth == null) {
+//			throw new AuthException("Exception message from AOP on unauthorized access NULL");
+//		} {
+//			if (!xAuth.equals("Willy De Keyser")) {
+//				throw new AuthException("Exception message from AOP on unauthorized access");
+//			}
+//		}
 	}
 	
-	//@Around("execution(* willydekeyser.controller.*.*(..)) && target(service)")
+	@Around("execution(* willydekeyser.controller.*.*(..)) && target(service)")
 	public Object logServiceAccess(ProceedingJoinPoint joinPoint, Object service) throws Throwable {
         long startTime = System.currentTimeMillis();
         Object result = joinPoint.proceed();
@@ -95,4 +98,16 @@ public class LoggingAspect {
 
     }
 	
+	//@Around("execution(* willydekeyser.controller.LedenController.*(..))")
+    public Object methodPointcut(ProceedingJoinPoint jointPoint) throws Throwable{
+		//Object result = new Object();
+		System.out.println("Voor proceed: " + jointPoint.toString());
+		try {
+			//result = (ModelAndView) jointPoint.proceed();
+		} finally {
+			
+		}
+		System.out.println("Na proceed: " + jointPoint.toString());
+		return "redirect:/testen.html";   
+    }
 }
