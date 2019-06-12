d8b82c05b94e390f050ed4d116484756f4551cb4
Willy De Keyser <willy.de.keyser@skynet.be>
2019-06-12 14:13:25 +0200

    Update start.js

--- src/main/java/willydekeyser/controller/LedenController.java
+++ src/main/java/willydekeyser/controller/LedenController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -219,8 +220,8 @@ public class LedenController {
 	}
 	
 	@GetMapping(path="/restcontroller/leden")
-	public @ResponseBody List<Leden> restConrollerLeden() {	
-		return ledenservice.getAllLeden();
+	public ResponseEntity<List<Leden>> restConrollerLeden() {
+		return ResponseEntity.ok().body(ledenservice.getAllLeden());
 	}
 	
 	@GetMapping(path="/restcontroller/ledenlijst")
--- src/main/resources/static/js/start.js
+++ src/main/resources/static/js/start.js
@@ -12,10 +12,10 @@ function start_main() {
 	console.log("Start main");
 	//initSessionMonitor();
 	document.getElementById('div_script').innerHTML = '';
-	$(document).on('contextmenu', (event) => {
+	document.oncontextmenu = (event) => {
 		console.log("Context menu is uitgeschakeld");
 		event.preventDefault();
-	});
+	};
 	section_height(80);
 	menu_main_width(300);
 	let promises = [Refrech_HTML('/start_menu_logo', '.header_logo'), 
@@ -30,7 +30,7 @@ function start_main() {
 
 function time_out() {
 	var sessionExpiry = Math.abs(getCookie('sessionExpiry'));
-    var timeOffset = Math.abs(getCookie('serverTime'));
+    var serverTime = Math.abs(getCookie('serverTime'));
     var localTime = (new Date()).getTime();
     
     //console.log("SessionExpiry: " + msToTime(sessionExpiry));
@@ -39,14 +39,14 @@ function time_out() {
     //console.log("Time out: " + msToTime(timeOut));
        
     timeOut = 0;
-    if((sessionExpiry - timeOffset) <= 0){
+    if((sessionExpiry - serverTime) <= 0){
     //if((sessionExpiry - localTime) <= 0){
     	clearInterval(timeOutTimer);
     } else {
     	timeOut = sessionExpiry - localTime;
     }  
     
-    document.getElementsByClassName('footer_section_A')[0].innerHTML = "Time out: " + msToTime(timeOut) + " - " + msToTime(timeOffset) + " - " + msToTime(sessionExpiry) + " - " + msToTime(localTime);
+    document.getElementsByClassName('footer_section_A')[0].innerHTML = "Time out: " + msToTime(timeOut) + " - " + msToTime(serverTime) + " - " + msToTime(sessionExpiry) + " - " + msToTime(localTime);
     document.getElementById('timeOutTeller').innerHTML = msToTime(timeOut);
     
     if(timeOut < 30000 && timeOut > 29000 ) {
@@ -107,13 +107,27 @@ function msToTime(duration) {
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-    //return minutes + ":" + seconds;
-    return hours + ":" + minutes + ":" + seconds;
+    return minutes + ":" + seconds;
+    //return hours + ":" + minutes + ":" + seconds;
 }
 
 function start_menu() {
+	var header_leden;
+	var header_leden_tabel;
+	var header_lidgeld;
+	var header_lidgeld_tabel;
+	var header_kasboek;
+	var header_kasboek_tabel;
+	var header_agenda;
+	var header_soortenleden;
+	var header_rubriek;
+	var header_restcontroller;
+	
 	timeOutTimer = setInterval('time_out()', 1000);
-	document.getElementById('header_leden').addEventListener('click', (event) => {
+	
+	header_leden = document.getElementById('header_leden');
+	if(header_leden != null) {
+		header_leden.addEventListener('click', (event) => {
 		event.preventDefault();
 		leden_start()
 		.then(() => {
@@ -123,96 +137,134 @@ function start_menu() {
 			console.error('Error in leden laden: ' + error);
 		});
 	});
-	document.getElementById('header_leden_tabel').addEventListener('click', (event) => {
-		event.preventDefault();
-		leden_tabel_start()
-		.then(() => {
-			console.log('Leden_tabel geladen');
-		})
-		.catch((error) => {
-			console.error('Error in leden_tabel laden: ' + error);
+	};
+
+	header_leden_tabel = document.getElementById('header_leden_tabel');
+	if(header_leden_tabel != null) {
+		header_leden_tabel.addEventListener('click', (event) => {
+			event.preventDefault();
+			leden_tabel_start()
+			.then(() => {
+				console.log('Leden_tabel geladen');
+			})
+			.catch((error) => {
+				console.error('Error in leden_tabel laden: ' + error);
+			});
 		});
-	});
-	document.getElementById('header_lidgeld').addEventListener('click', (event) => {
-		event.preventDefault();
-		max_lidgeld_start()
-		.then(() => {
-			console.log('Max_Lidgeld geladen');
-		})
-		.catch((error) => {
-			console.error('Error in leden_tabel laden: ' + error);
+	};
+	
+	header_lidgeld = document.getElementById('header_lidgeld');
+	if(header_lidgeld != null) {
+		header_lidgeld.addEventListener('click', (event) => {
+			event.preventDefault();
+			max_lidgeld_start()
+			.then(() => {
+				console.log('Max_Lidgeld geladen');
+			})
+			.catch((error) => {
+				console.error('Error in leden_tabel laden: ' + error);
+			});
 		});
-	});
-	document.getElementById('header_lidgeld_tabel').addEventListener('click', (event) => {
-		event.preventDefault();
-		max_lidgeld_start()
-		.then(() => {
-			console.log('Lidgeld_tabel geladen');
-		})
-		.catch((error) => {
-			console.error('Error in leden_tabel laden: ' + error);
+	};	
+	
+	header_lidgeld_tabel = document.getElementById('header_lidgeld_tabel');
+	if(header_lidgeld_tabel != null) {
+		header_lidgeld_tabel.addEventListener('click', (event) => {
+			event.preventDefault();
+			max_lidgeld_start()
+			.then(() => {
+				console.log('Lidgeld_tabel geladen');
+			})
+			.catch((error) => {
+				console.error('Error in leden_tabel laden: ' + error);
+			});
 		});
-	});
-	document.getElementById('header_kasboek').addEventListener('click', (event) => {
-		event.preventDefault();
-		kasboek_start()
-		.then(() => {
-			console.log('Kasboek geladen');
-		})
-		.catch((error) => {
-			console.error('Error in kasboek laden: ' + error);
+	};	
+	
+	header_kasboek = document.getElementById('header_kasboek');
+	if(header_kasboek != null) {
+		header_kasboek.addEventListener('click', (event) => {
+			event.preventDefault();
+			kasboek_start()
+			.then(() => {
+				console.log('Kasboek geladen');
+			})
+			.catch((error) => {
+				console.error('Error in kasboek laden: ' + error);
+			});
 		});
-	});
-	document.getElementById('header_kasboek_tabel').addEventListener('click', (event) => {
-		event.preventDefault();
-		max_lidgeld_start()
-		.then(() => {
-			console.log('Kasboek_tabel geladen');
-		})
-		.catch((error) => {
-			console.error('Error in kasboek_tabel laden: ' + error);
+	};	
+	
+	header_kasboek_tabel = document.getElementById('header_kasboek_tabel');
+	if(header_kasboek_tabel != null) {
+		header_kasboek_tabel.addEventListener('click', (event) => {
+			event.preventDefault();
+			max_lidgeld_start()
+			.then(() => {
+				console.log('Kasboek_tabel geladen');
+			})
+			.catch((error) => {
+				console.error('Error in kasboek_tabel laden: ' + error);
+			});
 		});
-	});
-	document.getElementById('header_agenda').addEventListener('click', (event) => {
-		event.preventDefault();
-		agenda_start()
-		.then(() => {
-			console.log('agenda geladen');
-		})
-		.catch((error) => {
-			console.error('Error in agenda laden: ' + error);
+	};
+	
+	header_agenda = document.getElementById('header_agenda');
+	if(header_agenda != null) {
+		header_agenda.addEventListener('click', (event) => {
+			event.preventDefault();
+			agenda_start()
+			.then(() => {
+				console.log('agenda geladen');
+			})
+			.catch((error) => {
+				console.error('Error in agenda laden: ' + error);
+			});
 		});
-	});
-	document.getElementById('header_soortenleden').addEventListener('click', (event) => {
-		event.preventDefault();
-		soortenleden_start()
-		.then(() => {
-			console.log('Soortenleden geladen');
-		})
-		.catch((error) => {
-			console.error('Error in soortenleden laden: ' + error);
+	};
+	
+	header_soortenleden = document.getElementById('header_soortenleden');
+	if(header_soortenleden != null) {
+		header_soortenleden.addEventListener('click', (event) => {
+			event.preventDefault();
+			soortenleden_start()
+			.then(() => {
+				console.log('Soortenleden geladen');
+			})
+			.catch((error) => {
+				console.error('Error in soortenleden laden: ' + error);
+			});
 		});
-	});
-	document.getElementById('header_rubriek').addEventListener('click', (event) => {
-		event.preventDefault();
-		rubriek_start()
-		.then(() => {
-			console.log('Rubriek_tabel geladen');
-		})
-		.catch((error) => {
-			console.error('Error in rubriek laden: ' + error);
+	};	
+	
+	header_rubriek = document.getElementById('header_rubriek');
+	if(header_rubriek != null) {
+		header_rubriek.addEventListener('click', (event) => {
+			event.preventDefault();
+			rubriek_start()
+			.then(() => {
+				console.log('Rubriek_tabel geladen');
+			})
+			.catch((error) => {
+				console.error('Error in rubriek laden: ' + error);
+			});
 		});
-	});
-	document.getElementById('header_restcontroller').addEventListener('click', (event) => {
-		event.preventDefault();
-		restcontroller_start()
-		.then(() => {
-			console.log('Restcontroller geladen');
-		})
-		.catch((error) => {
-			console.error('Error in restcontroller laden: ' + error);
+	};	
+	
+	header_restcontroller = document.getElementById('header_restcontroller');
+	if(header_restcontroller != null) {
+		header_restcontroller.addEventListener('click', (event) => {
+			event.preventDefault();
+			restcontroller_start()
+			.then(() => {
+				console.log('Restcontroller geladen');
+			})
+			.catch((error) => {
+				console.error('Error in restcontroller laden: ' + error);
+			});
 		});
-	});
+	};	
+	
 	console.log('Menu header onclick geladen');
 };
 
