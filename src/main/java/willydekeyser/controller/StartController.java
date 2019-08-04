87f3f8f748fce46e9c778cd3adbffe4e3c517b3c
Willy De Keyser <willy.de.keyser@skynet.be>
2019-08-04 17:47:16 +0200

    Update 08-08-2019

--- src/main/java/willydekeyser/controller/StartController.java
+++ src/main/java/willydekeyser/controller/StartController.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -131,7 +132,7 @@ public class StartController {
  *
  * 
  */	
-
+	@Secured("ROLE_GOLD")
 	@GetMapping("/restcontroller")
 	public @ResponseBody String index() {	
 		String index = "<p><br/></p>"
